ad7c415a7d3d4b0776b71c73fa13e4c4c095485f
PingPuping <91452477+PingPuping@users.noreply.github.com>
2023-04-25 19:05:09 +0700

    My web port V1.0
    Add all page.

--- js/app.js
+++ js/app.js
@@ -7,11 +7,11 @@ const link = button.querySelector('a');
 // Add event listener for hover
 button.addEventListener('mouseover', () => {
   // Update the text of the link element
-  link.textContent = 'This is what i can do';
+  link.textContent = 'This is what i can do (>‿◠)';
 });
 
 // Add event listener for mouseout
 button.addEventListener('mouseout', () => {
   // Revert the text of the link element to the original text
-  link.textContent = 'Know what i can do.';
+  link.textContent = 'Know what i can do! (>‿◠)';
 });
